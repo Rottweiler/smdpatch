aca98d460f2f6e4ed44655fc1a144e40a53f1644
Rottweiler <rottweiler.hackhound@aol.com>
2015-01-10 23:53:21 +0100

    First commit

---
+++ Entrypoint.vb
@@ -0,0 +1,59 @@
+﻿Imports System.IO
+Imports System.Text
+
+Module Entrypoint
+
+    Sub Main(ByVal ParamArray args As String())
+        If args IsNot Nothing Then
+            If args.Length > 0 Then
+                If IO.File.Exists(args(0)) Then
+                    Dim data As Byte() = {}
+                    If CrowbarSignature(args(0)) Then
+                        Console.WriteLine("Detected Crowbar signature, patching..")
+                        If patch(data, args(0)) Then
+                            Dim fn As String = args(0).Substring(args(0).LastIndexOf("\") + 1)
+                            Dim workingdir As String = args(0).Replace("\" & fn, "")
+                            IO.File.WriteAllBytes(Path.Combine(workingdir, "smdpatch_" & fn), data)
+                            Console.WriteLine("Patched!")
+                        End If
+                    Else
+                        Console.WriteLine("No Crowbar signature detected.")
+                    End If
+                End If
+            End If
+        End If
+        Console.ReadLine()
+    End Sub
+
+    Function patch(ByRef data As Byte(), ByVal file As String) As Boolean
+        Using fs As New FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)
+            Using br As New BinaryReader(fs)
+                Dim bytes() As Byte = New Byte((br.BaseStream.Length) - 33) {}
+                fs.Position = 32
+                fs.Read(bytes, 0, bytes.Length)
+                data = bytes
+                Return True
+            End Using
+        End Using
+    End Function
+
+    Function CrowbarSignature(ByVal file As String) As Boolean
+        Using fs As New FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)
+            Using br As New BinaryReader(fs)
+                Dim bytes(32) As Byte
+                br.Read(bytes, 0, bytes.Length)
+                Dim str As String = UnChar(bytes)
+                Return str.Contains("Crowbar")
+            End Using
+        End Using
+    End Function
+
+    Function UnChar(ByVal Str As Byte()) As String
+        Dim sb As New StringBuilder
+        For Each C As Byte In Str
+            sb.Append(ChrW(C))
+        Next
+        Return sb.ToString
+    End Function
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("smdpatch")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("smdpatch")> 
+<Assembly: AssemblyCopyright("Copyright © Rottweiler 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5479f039-8852-47cc-a3d1-98e10f62148a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
